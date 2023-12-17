c406b2f6eccf92c50c14c321d2765d57f63cf630
dvmduc5269 <ducduvumanh@gmail.com>
2023-12-17 09:05:22 +0700

    Minor change

--- BT1/src/referee/Referee.java
+++ BT1/src/referee/Referee.java
@@ -12,9 +12,6 @@ public class Referee {
         System.out.println("Choose a player: ");
         Scanner scanner = new Scanner(System.in);
         int playerNumber = scanner.nextInt();
-        if (playerNumber < 1 || playerNumber > 4) {
-            throw new ArrayIndexOutOfBoundsException("Player number must be between 1 and 4!");
-        }
         return playerNumber;
     }
 
